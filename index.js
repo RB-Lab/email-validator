2cb9c535a8b888ccccc1577058a9f7c6a07ef464
Roman Bekkiev <rom.bekk@gmail.com>
2016-02-06 18:54:21 +0100

    fix expots

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function(expots){
+(function(exports){
 	"use strict";
 	
 	var tester = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-?\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
